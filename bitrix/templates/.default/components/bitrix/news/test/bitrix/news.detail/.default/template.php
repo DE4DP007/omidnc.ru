b9cfe1b2decd4f653c6c721e87556cfe7121210a
ismag <ismag@example.com>
2016-04-22 16:51:11 +0300

    publ detailed

--- bitrix/templates/.default/components/bitrix/news/test/bitrix/news.detail/.default/template.php
+++ bitrix/templates/.default/components/bitrix/news/test/bitrix/news.detail/.default/template.php
@@ -12,84 +12,63 @@
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 ?>
-<div class="news-detail">
-	<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arResult["DETAIL_PICTURE"])):?>
-		<img
-			class="detail_picture"
-			border="0"
-			src="<?=$arResult["DETAIL_PICTURE"]["SRC"]?>"
-			width="<?=$arResult["DETAIL_PICTURE"]["WIDTH"]?>"
-			height="<?=$arResult["DETAIL_PICTURE"]["HEIGHT"]?>"
-			alt="<?=$arResult["DETAIL_PICTURE"]["ALT"]?>"
-			title="<?=$arResult["DETAIL_PICTURE"]["TITLE"]?>"
-			/>
-	<?endif?>
-	<?if($arParams["DISPLAY_DATE"]!="N" && $arResult["DISPLAY_ACTIVE_FROM"]):?>
-		<span class="news-date-time"><?=$arResult["DISPLAY_ACTIVE_FROM"]?></span>
-	<?endif;?>
-	<?if($arParams["DISPLAY_NAME"]!="N" && $arResult["NAME"]):?>
-		<h3><?=$arResult["NAME"]?></h3>
-	<?endif;?>
-	<?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arResult["FIELDS"]["PREVIEW_TEXT"]):?>
-		<p><?=$arResult["FIELDS"]["PREVIEW_TEXT"];unset($arResult["FIELDS"]["PREVIEW_TEXT"]);?></p>
-	<?endif;?>
-	<?if($arResult["NAV_RESULT"]):?>
-		<?if($arParams["DISPLAY_TOP_PAGER"]):?><?=$arResult["NAV_STRING"]?><br /><?endif;?>
-		<?echo $arResult["NAV_TEXT"];?>
-		<?if($arParams["DISPLAY_BOTTOM_PAGER"]):?><br /><?=$arResult["NAV_STRING"]?><?endif;?>
-	<?elseif(strlen($arResult["DETAIL_TEXT"])>0):?>
-		<?echo $arResult["DETAIL_TEXT"];?>
-	<?else:?>
-		<?echo $arResult["PREVIEW_TEXT"];?>
-	<?endif?>
-	<div style="clear:both"></div>
-	<br />
-	<?foreach($arResult["FIELDS"] as $code=>$value):
-		if ('PREVIEW_PICTURE' == $code || 'DETAIL_PICTURE' == $code)
-		{
-			?><?=GetMessage("IBLOCK_FIELD_".$code)?>:&nbsp;<?
-			if (!empty($value) && is_array($value))
-			{
-				?><img border="0" src="<?=$value["SRC"]?>" width="<?=$value["WIDTH"]?>" height="<?=$value["HEIGHT"]?>"><?
-			}
-		}
-		else
-		{
-			?><?=GetMessage("IBLOCK_FIELD_".$code)?>:&nbsp;<?=$value;?><?
-		}
-		?><br />
-	<?endforeach;
-	foreach($arResult["DISPLAY_PROPERTIES"] as $pid=>$arProperty):?>
+<div class="news-detail ">
+    <?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arResult["DETAIL_PICTURE"])):?>
+        <img
+            class="detail_picture img-rounded img-thumbnail"
+            border="0"
+            src="<?=$arResult["DETAIL_PICTURE"]["SRC"]?>"
+            width="<?=$arResult["DETAIL_PICTURE"]["WIDTH"]?>"
+            height="<?=$arResult["DETAIL_PICTURE"]["HEIGHT"]?>"
+            alt="<?=$arResult["DETAIL_PICTURE"]["ALT"]?>"
+            title="<?=$arResult["DETAIL_PICTURE"]["TITLE"]?>"
+        />
+    <?endif?>
 
-		<?=$arProperty["NAME"]?>:&nbsp;
-		<?if(is_array($arProperty["DISPLAY_VALUE"])):?>
-			<?=implode("&nbsp;/&nbsp;", $arProperty["DISPLAY_VALUE"]);?>
-		<?else:?>
-			<?=$arProperty["DISPLAY_VALUE"];?>
-		<?endif?>
-		<br />
-	<?endforeach;
-	if(array_key_exists("USE_SHARE", $arParams) && $arParams["USE_SHARE"] == "Y")
-	{
-		?>
-		<div class="news-detail-share">
-			<noindex>
-			<?
-			$APPLICATION->IncludeComponent("bitrix:main.share", "", array(
-					"HANDLERS" => $arParams["SHARE_HANDLERS"],
-					"PAGE_URL" => $arResult["~DETAIL_PAGE_URL"],
-					"PAGE_TITLE" => $arResult["~NAME"],
-					"SHORTEN_URL_LOGIN" => $arParams["SHARE_SHORTEN_URL_LOGIN"],
-					"SHORTEN_URL_KEY" => $arParams["SHARE_SHORTEN_URL_KEY"],
-					"HIDE" => $arParams["SHARE_HIDE"],
-				),
-				$component,
-				array("HIDE_ICONS" => "Y")
-			);
-			?>
-			</noindex>
-		</div>
-		<?
-	}
-	?>
+    <?if($arParams["DISPLAY_NAME"]!="N" && $arResult["NAME"]):?>
+        <h2 class="text-center"><?=$arResult["NAME"]?></h2>
+    <?endif;?>
+
+    <h4 class="text-center">
+        <?= $arResult["DISPLAY_PROPERTIES"]["AUTHORS"]["DISPLAY_VALUE"]?>
+    </h4>
+
+    <div class="row">
+        <p class="col-sm-6 col-xs-12 text-left">
+            <b>УДК:&nbsp;  <?= $arResult["PROPERTIES"]["UDK"]["VALUE"]?></b>
+        </p>
+
+        <p class="col-sm-6 col-xs-12 text-right">
+            <b><?= $arResult["DISPLAY_PROPERTIES"]["PUBLTYPE"]["DISPLAY_VALUE"]?></b>
+        </p>
+    </div>
+
+
+    <p class="text-justify">
+        <?=$arResult["DETAIL_TEXT"];?>
+    </p>
+
+    <div class="row">
+        <p class="text-right col-md-12"><b>
+                <?= $arResult["PROPERTIES"]["PUBLDATE"]["NAME"]?>:&nbsp;
+                <?= $arResult["PROPERTIES"]["PUBLDATE"]["VALUE"]?>
+        </b></p>
+    </div><br/>
+    <p><b>
+        <?= $arResult["PROPERTIES"]["BIBLIODATA"]["NAME"]?>:&nbsp;</b><br/>
+        <?= $arResult["PROPERTIES"]["BIBLIODATA"]["VALUE"]?>
+    </p><br><br>
+
+    <h5 class="text-center">
+        <? $pather = $arResult["DISPLAY_PROPERTIES"]["FULLTEXT"]["DISPLAY_VALUE"];
+           ?>
+        <a href="<?=$pather?>"
+        class="btn btn-primary btn-large"><b>
+                <span class="glyphicon glyphicon-save"></span>&nbsp;
+                СКАЧАТЬ ПОЛНЫЙ ТЕКСТ
+        </b></a>
+    </h5>
+    <br><br>
+
+
 </div>
